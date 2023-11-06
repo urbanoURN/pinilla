af4202d62f7f3eefe7be7187f221cb4724e81f40
Harold2645 <supapito85@gmail.com>
2023-11-06 16:30:22 -0500

    V 6.0

--- js.js
+++ js.js
@@ -1,29 +1,29 @@
 function sumar() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = num1 + num2;
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 + numero2;
     document.getElementById("resultado").textContent = resultado;
 }
 
 function restar() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = num1 - num2;
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 - numero2;
     document.getElementById("resultado").textContent = resultado;
 }
 
 function multiplicar() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = num1 * num2;
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 * numero2;
     document.getElementById("resultado").textContent = resultado;
 }
 
 function dividir() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    if (num2 !== 0) {
-        var resultado = num1 / num2;
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    if (numero2 !== 0) {
+        var resultado = numero1 / numero2;
         document.getElementById("resultado").textContent = resultado;
     } else {
         document.getElementById("resultado").textContent = "Error: División por cero.";
